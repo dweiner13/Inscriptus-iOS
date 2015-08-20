6b4a7cd026eceaa0a2a38d6987871912381d2baf
Daniel Weiner <daniel.weiner@me.com>
2015-08-20 14:29:44 -0400

    remove whitaker cache logging

--- inscriptus/WhitakerCache.swift
+++ inscriptus/WhitakerCache.swift
@@ -20,8 +20,6 @@ class WhitakerCache {
         var storedCache: NSData? = defaults.objectForKey(DEFAULTS_CACHE_KEY) as! NSData?
         if let savedData = storedCache {
             self.items = NSKeyedUnarchiver.unarchiveObjectWithData(savedData) as! Set<WhitakerResult>
-            println("Found saved cache, logging:")
-            println(self.items)
         }
         else {
             self.items = Set<WhitakerResult>()
